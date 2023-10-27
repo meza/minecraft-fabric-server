95faa2096a403a20665d83dc69c6bc8ca42f7481
Meza <meza@users.noreply.github.com>
2023-10-27 03:36:05 +0100

    ci: fixed the fabric issue

--- scripts/install-fabric.sh
+++ scripts/install-fabric.sh
@@ -20,7 +20,7 @@ echo Latest loader version: "$LATEST_LOADER_VERSION"
 
 LAUNCHER_URL="https://meta.fabricmc.net/v2/versions/loader/${MINECRAFT_VERSION}/${LATEST_LOADER_VERSION}/${LATEST_INSTALLER_VERSION}/server/jar"
 
-if curl --output /dev/null --silent --head --fail "$url"; then
+if curl --output /dev/null --silent --head --fail "$LAUNCHER_URL"; then
   echo "There is sa fabric launcher for MC${MINECRAFT_VERSION}";
 else
   echo "There is no fabric launcher for this MC${MINECRAFT_VERSION} yet";
