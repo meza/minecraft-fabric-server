d0f877f2ad4aecbe070c00ccb77c5c00ef59757f
Meza <meza@users.noreply.github.com>
2023-12-01 13:06:35 +0000

    feat: added github token support

--- scripts/mmmInstall.sh
+++ scripts/mmmInstall.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-curl -s "https://api.github.com/repos/meza/minecraft-mod-manager/releases/latest" | \
+URL="https://api.github.com/repos/meza/minecraft-mod-manager/releases/latest"
+
+
+if [ -z "$GITHUB_TOKEN" ]; then
+  JSON=$(curl -s --request GET --url "$URL")
+else
+  JSON=$(curl -s --request GET --url "$URL" \
+  --header "X-GitHub-Api-Version: 2022-11-28" \
+  --header "Authorization: Bearer $GITHUB_TOKEN")
+fi
+
+echo $JSON | \
 jq -r '.assets[] | select(.label | contains("Linux")) | .browser_download_url' | \
 xargs wget -O /tmp/mmm.zip && \
 unzip -o /tmp/mmm.zip -d /minecraft/server
