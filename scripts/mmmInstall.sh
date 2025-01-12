3cd7d0582dd03b252107b8ba2afecefdaec91b38
meza <211977+meza@users.noreply.github.com>
2025-01-12 23:50:05 +0000

    feat: added MMM_URL env variable to control specific version of mmm to download

--- scripts/mmmInstall.sh
+++ scripts/mmmInstall.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 URL="https://api.github.com/repos/meza/minecraft-mod-manager/releases/latest"
 
-
-if [ -z "$GITHUB_TOKEN" ]; then
-  JSON=$(curl -s --request GET --url "$URL")
+if [ -n "$MMM_URL" ]; then
+  DOWNLOAD_URL="$MMM_URL"
 else
-  JSON=$(curl -s --request GET --url "$URL" \
-  --header "X-GitHub-Api-Version: 2022-11-28" \
-  --header "Authorization: Bearer $GITHUB_TOKEN")
+  if [ -z "$GITHUB_TOKEN" ]; then
+    JSON=$(curl -s --request GET --url "$URL")
+  else
+    JSON=$(curl -s --request GET --url "$URL" \
+    --header "X-GitHub-Api-Version: 2022-11-28" \
+    --header "Authorization: Bearer $GITHUB_TOKEN")
+  fi
+
+  DOWNLOAD_URL=$(echo $JSON | jq -r '.assets[] | select(.label | contains("Linux")) | .browser_download_url')
 fi
 
-echo $JSON | \
-jq -r '.assets[] | select(.label | contains("Linux")) | .browser_download_url' | \
-xargs wget -O /tmp/mmm.zip && \
+wget -O /tmp/mmm.zip "$DOWNLOAD_URL" && \
 unzip -o /tmp/mmm.zip -d /minecraft/server
